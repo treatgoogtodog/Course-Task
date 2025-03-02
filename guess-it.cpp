c8d6418410e4de5ac2608e35599edb81bcb5d4e4
treatgoogtodog <70848404+treatgoogtodog@users.noreply.github.com>
2025-03-02 12:09:27 +0700

    Create guess-it.cpp
    "Meh"

---
+++ guess-it.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <random>
+#include <string>
+
+using namespace std;
+
+int getNumber();
+
+string getAnswer();
+
+int getGuesses();
+
+void rigged_mode();
+
+int main(){
+
+    cout<<"experiencial rigged mode? (y/n)";
+
+    while (true)
+    {
+        
+    }
+    
+}
